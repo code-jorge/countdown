cf112888b38fe36f12455e57f66df0f79ec3b596
Jorge Aguirre <jorge.aguirre.leon@gmail.com>
2024-05-10 17:01:07 +0200

    Small fix

--- netlify/edge-functions/date.js
+++ netlify/edge-functions/date.js
@@ -3,7 +3,7 @@ export default async function handler(_request, context) {
   const headers = response.headers;
   if (headers.get('content-type').includes('text/html')) {
     const content = await response.text();
-    const date = Netlify.get("TARGET_DATE")
+    const date = Netlify.env.get("TARGET_DATE")
     const updatedContent = content.replace('const referenceDate = ""', `const referenceDate = "${date}"`)
     return new Response(updatedContent, response);
   }
