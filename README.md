6fbf6402ef13798993460ade2d874147b09d0e70
Jorge Aguirre <jorge.aguirre.leon@gmail.com>
2024-05-10 16:58:21 +0200

    Small fix

---
+++ README.md
@@ -0,0 +1,5 @@
+## Countdown
+
+[![Netlify Status](https://api.netlify.com/api/v1/badges/61d86346-bca1-4376-9bfc-df5c350f28be/deploy-status)](https://app.netlify.com/sites/jorge-is-counting-down/deploys)
+
+Uses `Edge Functions` to set the `TARGET_DATE` environment variable into our HTML.
--- netlify/edge-functions/date.js
+++ netlify/edge-functions/date.js
@@ -1,11 +1,10 @@
-const TARGET_DATE = process.env.TARGET_DATE;
-
 export default async function handler(_request, context) {
   const response = await context.next();
   const headers = response.headers;
   if (headers.get('content-type').includes('text/html')) {
     const content = await response.text();
-    const updatedContent = content.replace('const referenceDate = ""', `const referenceDate = "${TARGET_DATE}"`)
+    const date = Netlify.get("TARGET_DATE")
+    const updatedContent = content.replace('const referenceDate = ""', `const referenceDate = "${date}"`)
     return new Response(updatedContent, response);
   }
   return response;
